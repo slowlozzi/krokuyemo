e15eb3fba4a823ba3e68a8db52ac6d7d4e331d91
slowlozzi <123765509+slowlozzi@users.noreply.github.com>
2023-02-11 21:17:50 +0200

    Update and rename razminka.js to krokuyemo.js

---
+++ krokuyemo.js
@@ -0,0 +1,8 @@
+for ( let step= 0; step < 5; step++) {
+  console.log('Йдемо 1 крок');
+  console.log('Йдемо 2 кроки');
+  console.log('Йдемо 3 кроки');
+  console.log('Йдемо 4 кроки');
+  console.log('Йдемо 5 кроків');
+  console.log('Йдемо 6 кроків');
+}
